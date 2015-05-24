c7626d7142d909d6ed0879c219ab21dba9edf26a
Avishaan <avishaan@codeHatcher.com>
2015-05-24 16:03:47 -0700

    add own label interface controller and connect to storyboard

---
+++ WatchKitUI WatchKit Extension/LabelInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  LabelInterfaceController.swift
+//  WatchKitUI
+//
+//  Created by Brown Magic on 5/24/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class LabelInterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
